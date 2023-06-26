02c52d71fc3a646512bd4e0ba84a649a7d5c044b
vangelif <ev.fotiades@yahoo.com>
2023-06-26 19:38:51 +0300

    scaffold components for UI

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import './App.scss';
 import { Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Navigation from './components/Navigation';
 import Homepage from './components/Homepage';
+import { fetchCountries } from './redux/countriesSlice';
+import CountryItems from './components/CountryItems';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCountries());
+  }, [dispatch]);
+
   return (
     <div className="App">
       <Navigation />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="countries">
-          <Route path="australia" />
+          <Route path=":countryName" element={<CountryItems />} />
         </Route>
       </Routes>
     </div>
