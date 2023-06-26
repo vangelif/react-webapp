ad3d525bf97ae9e609424e6312b2b8788c744223
vangelif <ev.fotiades@yahoo.com>
2023-06-26 12:12:52 +0300

    build react setup and linters

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+const Nav = () => <div>hellooooo</div>;
+
+function App() {
+  return (
+    <div className="App">
+      hi
+      <Nav />
+      <header className="App-header">hello world</header>
+    </div>
+  );
+}
+
+export default App;
