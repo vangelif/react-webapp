6cf3ddfa72f8479497bdb33c8d60c9da65f26700
vangelif <ev.fotiades@yahoo.com>
2023-06-29 16:24:42 +0300

    build components tests

---
+++ src/tests/Navigation.test.js
@@ -0,0 +1,28 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import myCountries from './Countries';
+import Navigation from '../components/Navigation';
+
+const reducer = (
+  state = {
+    countries: { countries: myCountries },
+  }
+) => state;
+
+const store = configureStore({ reducer });
+describe('Navigation test', () => {
+  it('will test if Navigation is rendering correctly', () => {
+    const tree = render(
+      <MemoryRouter>
+        <Provider store={store}>
+          <Navigation />
+        </Provider>
+      </MemoryRouter>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
