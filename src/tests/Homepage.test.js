005380193e825f8268fc248aed96746113c07596
vangelif <ev.fotiades@yahoo.com>
2023-06-30 17:39:21 +0300

    scaffold tests for the components

--- src/tests/Homepage.test.js
+++ src/tests/Homepage.test.js
@@ -1,12 +1,13 @@
 import '@testing-library/jest-dom';
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import { configureStore } from '@reduxjs/toolkit';
-import Homepage from '../components/Homepage';
+// import Homepage1 from '../components/Homepage1';
 import myCountries from './Countries';
+import Homepage from '../components/Homepage';
 
 const reducer = (
   state = {
@@ -18,13 +19,16 @@ const store = configureStore({ reducer });
 
 describe('Homepage test', () => {
   it('will test if Homepage is rendering correctly', () => {
-    const tree = render(
+    render(
       <MemoryRouter>
         <Provider store={store}>
           <Homepage />
         </Provider>
       </MemoryRouter>
     );
-    expect(tree).toMatchSnapshot();
+    const title = screen.getByAltText('World Map');
+    expect(title).toBeInTheDocument();
+    const mainTitle = screen.getByText('World Map Statistics');
+    expect(mainTitle).toBeInTheDocument();
   });
 });
